b2271ad53a1cb64ea20c3e70bd92f0336e9f7842
Andre Etienne <andreetienne1987@gmail.com>
2015-08-27 10:17:34 +0200

    Add mobileNavigation.php and stuff for default Template

--- app/views/templates/default.php
+++ app/views/templates/default.php
@@ -1,13 +1,17 @@
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Website | {% block title %}{% endblock %}</title>
+    <link rel="stylesheet" href="../app/views/templates/Public/css/style.min.css" media="screen" />
 </head>
     <body>
-    {% include 'templates/partials/messages.php' %} 
+    {% include 'templates/partials/messages.php' %}
     {% include 'templates/partials/navigation.php' %}
     {% block content%} {% endblock %}
     </body>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="text/javascript" src="../app/views/templates/Public/js/main.min.js"></script>
 </html>
